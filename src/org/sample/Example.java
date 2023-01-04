9affadab0f96dec7934be17731d7890f748edb2e
harishchelian <harishchelian1996@gmail.com>
2023-01-04 12:42:09 +0530

    Code added by harish

--- src/org/sample/Example.java
+++ src/org/sample/Example.java
@@ -2,8 +2,12 @@ package org.sample;
 
 public class Example {
 	
+	private void openBrowser() {
+		System.out.println("Open Browser");
+
+	}
     public static void main(String[] args) {
-		
+    	
     	System.out.println("Hi Im Harish");
 	}
 
